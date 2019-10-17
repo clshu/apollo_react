d1c637091a4a911c442f47945d7ad00e15c2022f
clshu <ccss0630@yahoo.com>
2019-10-17 15:31:39 -0700

    Remove old files and create a new App with a react router path '/' and '/songs/new' set up

---
+++ src/components/SongCreate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongCreate = () => {
+  return (
+    <div>
+      <form>
+        <lable>Create A New Song</lable>
+        <input />
+      </form>
+    </div>
+  );
+};
+
+export default SongCreate;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+//import { gql } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+const App = () => {
+  // client
+  //   .query({
+  //     query: gql`
+  //       {
+  //         songs {
+  //           id
+  //           title
+  //         }
+  //       }
+  //     `
+  //   })
+  //   .then(result => console.log(result));
+
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <SongList />
+          </Route>
+          <Route path="/songs/new">
+            <SongCreate />
+          </Route>
+        </Switch>
+      </Router>
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
